2a629b9bb54b2119b3704ed0ce167180c60b3093
Fabian Menn <fabi@Fabians-iMac.fritz.box>
2017-08-25 20:31:56 +0200

    email authentification

--- MySocial/SignInVC.swift
+++ MySocial/SignInVC.swift
@@ -13,14 +13,13 @@ import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var passwordField: FancyField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func facebookBtnPressed(_ sender: Any) {
@@ -48,6 +47,25 @@ class SignInVC: UIViewController {
             } else {
                 print("FABIAN: .... Successfully authenticated with Firebase")
             }
+        }
+    }
+    
+    @IBAction func signInTapped(_ sender: Any) {
+        
+        if let email = emailField.text, let password = passwordField.text {
+            Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error == nil {
+                    print("FABIAN: Email User authenticated with Firebase")
+                } else {
+                    Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if error != nil {
+                            print("FABIAN: Unable to authenticate with Firebase using email")
+                        } else {
+                            print("FABIAN: Successfully authenticated with Firebase using email")
+                        }
+                    })
+                }
+            })
         }
     }
 

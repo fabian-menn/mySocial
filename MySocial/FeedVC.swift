4eb2c4ef8fccaf73f3cd01ca240ea9bf178bc39c
Fabian Menn <fabi@Fabians-iMac.fritz.box>
2017-08-27 12:30:03 +0200

    table view set up

--- MySocial/FeedVC.swift
+++ MySocial/FeedVC.swift
@@ -10,12 +10,27 @@ import UIKit
 import SwiftKeychainWrapper
 import Firebase
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCellTableViewCell
     }
     
     @IBAction func signOutPressed(_ sender: Any) {
@@ -24,5 +39,7 @@ class FeedVC: UIViewController {
         try! Auth.auth().signOut()
         performSegue(withIdentifier: "toSignInVC", sender: nil)
     }
+    
+    
 
 }
---
+++ MySocial/PostCellTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCellTableViewCell.swift
+//  MySocial
+//
+//  Created by Fabian Menn on 8/27/17.
+//  Copyright © 2017 Fabian Menn. All rights reserved.
+//
+
+import UIKit
+
+class PostCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}
